c3b7800cae6f7bddc4e3d805692ea22b5281092e
Frank Morton <fmorton@mac.com>
2022-11-01 10:48:48 -0400

    fix extlinks in docs/conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -25,8 +25,8 @@ version = release = '0.0.9'
 pygments_style = 'trac'
 templates_path = ['.']
 extlinks = {
-    'issue': ('https://github.com/fmorton/python-hummingbird-robot/issues/%s', '#'),
-    'pr': ('https://github.com/fmorton/python-hummingbird-robot/pull/%s', 'PR #'),
+    'issue': ('https://github.com/fmorton/python-hummingbird-robot/issues/%s', 'issue #%s'),
+    'pr': ('https://github.com/fmorton/python-hummingbird-robot/pull/%s', 'PR #%s'),
 }
 # on_rtd is whether we are on readthedocs.org
 on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
